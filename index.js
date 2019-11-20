10dfca1b9f40192397efb9597176fc2f9019edac
Santiago Moran <santimoran.1@hotmail.com>
2019-11-20 01:34:30 -0300

    Cors and production mode enableds

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ require('dotenv').config();
 
 const { makeExecutableSchema } = require('graphql-tools');
 const express = require('express');
+const cors = require('cors');
 const gqlMiddleware = require('express-graphql');
 const { readFileSync } = require('fs');
 const { join } = require('path');
 
 const app = express()
 const port = process.env.port || 3000;
+const isDev = process.env.NODE_ENV !== 'production';
 
 const resolvers = require('./lib/resolvers');
 
@@ -21,10 +23,12 @@ const typeDefs = readFileSync(
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
+app.use(cors());
+
 app.use('/api', gqlMiddleware({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true
+    graphiql: isDev
 }));
 
 app.listen(port, _ => {
